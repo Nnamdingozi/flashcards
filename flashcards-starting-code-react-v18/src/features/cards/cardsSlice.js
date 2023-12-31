728de455ae0d71b5cc15d081f7b035b938c52b50
joerald <ngozika1105@gmail.com>
2023-12-31 10:48:10 +0100

    add slices to project

---
+++ flashcards-starting-code-react-v18/src/features/cards/cardsSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const cardsSlice = createSlice({
+    name: 'cards',
+    initialState: {
+        cards: {}
+    },
+    reducers: {
+        addCard: (state, action ) => {
+            const { id, front, back } = action.payload;
+            state.cards[id] = {id: id, front: front, back: back}
+        }
+    },
+    
+
+});
+export default cardsSlice.reducer;
+export const selectCardById = (id) => (state) => state.cards.cards[id]
+export const { addCard } = cardsSlice.actions;
